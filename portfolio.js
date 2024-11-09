a934aca30e7c4a68bd21de99ce38c97c4384fcc7
Esteban Sempere <90720500+Colte111@users.noreply.github.com>
2024-11-09 18:37:07 +0100

    09/11/2024
    Final

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,18 @@
 const navlink = document.querySelectorAll('.nav-link');
 const menuToggle = document.getElementById('menuToggle');
 const mobileMenu = document.getElementById('mobileMenu');
+const linkLang = document.querySelectorAll('.lang');
+
+const textFooter = document.getElementById('textFooter')
+
+const year = new Date().getFullYear()
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const frase = `© ${year} estebanSem. All rights reserved`
+
+    textFooter.innerHTML = frase;
+})
 
 // Añadir un evento de clic a cada enlace
 navlink.forEach(link => {
@@ -12,6 +24,41 @@ navlink.forEach(link => {
         this.classList.add('active');
     });
 });
+
+
+
+   
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll(".nav-link"); // Selecciona todos los enlaces de navegación
+    const sections = document.querySelectorAll(".section"); // Selecciona todas las secciones
+
+    function updateActiveLink() {
+        let currentSection = "";
+
+        // Revisa cada sección para determinar cuál está en la vista
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.offsetHeight;
+
+            if (window.scrollY >= sectionTop - sectionHeight / 3) {
+                currentSection = section.getAttribute("id");
+            }
+        });
+
+        // Elimina .active de todos los enlaces y añade .active al actual
+        navLinks.forEach((link) => {
+            link.classList.remove("active");
+            if (link.getAttribute("href") === `#${currentSection}`) {
+                link.classList.add("active");
+            }
+        });
+    }
+
+    // Evento para actualizar enlaces al hacer scroll
+    window.addEventListener("scroll", updateActiveLink);
+});
+
+ 
 
 
 menuToggle.addEventListener('click', () => {
